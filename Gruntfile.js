b110f8d24e9c7aeb811c54da8e41c19154be87a8
Kaspar Naaber <kaspar.naaber@outlook.com>
2016-04-15 12:05:06 +0300

    Update project structure and rerun Grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,27 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // Builds custom modernizr script.
-    modernizr: {
+    // Removes old files.
+    clean: {
+      reset: {
+        src: ['assets', 'images', 'javascripts', 'stylesheets']
+      },
+
+      remove: {
+        src: ['sources/components/custom-styles/tmp']
+      }
+    },
+
+    modernizr_builder: {
       build: {
-        'devFile' : 'bower_components/modernizr/modernizr.js',
-        'outputFile' : 'javascripts/modernizr.js',
-        'tests': [
-          'flexbox',
-          'svg'
-        ],
-        'uglify' : false
+        options: {
+          config: 'modernizr-config.json',
+          dest: 'javascripts/modernizr-custom.min.js',
+          uglify: true
+        }
       }
     },
 
@@ -23,7 +31,7 @@ module.exports = function(grunt) {
         src: [
           'bower_components/jquery/dist/jquery.js',
           'bower_components/textarea-autosize/dist/jquery.textarea_autosize.js',
-          'sources/javascripts/*.js'
+          'sources/javascripts/concat/*.js'
         ],
         dest: 'javascripts/main.js'
       }
@@ -47,7 +55,7 @@ module.exports = function(grunt) {
 
     // Compiles the stylesheet files.
     sass: {
-      build: {
+      build_main: {
         options: {
           style: 'expanded',
           sourcemap: 'none'
@@ -59,6 +67,40 @@ module.exports = function(grunt) {
           dest: 'stylesheets/',
           ext: '.css'
         }]
+      },
+
+      // Builds custom style components to temporary folder.
+      build_custom_styles: {
+        options: {
+          style: 'expanded',
+          sourcemap: 'none'
+        },
+        files: [{
+          expand: true,
+          cwd: 'sources/components/custom-styles',
+          src: '*.scss',
+          dest: 'sources/components/custom-styles/tmp',
+          ext: '.css'
+        }]
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({browsers: 'last 4 versions'})
+        ]
+      },
+      main_styles: {
+        src: [
+          'stylesheets/*.css',
+          'stylesheets/!*.min.css'
+        ]
+      },
+      custom_styles: {
+        src: [
+          'sources/components/custom-styles/tmp/*.css'
+        ]
       }
     },
 
@@ -78,26 +120,94 @@ module.exports = function(grunt) {
 
     // Minifies the image files.
     imagemin: {
-      build: {
+      build_images: {
         files: [{
           expand: true,
-          cwd: 'sources/images',
+          cwd: 'sources/images/minify',
           src: '*.{png,jpg,gif}',
           dest: 'images/'
+        }]
+      },
+
+      build_assets: {
+        files: [{
+          expand: true,
+          cwd: 'sources/assets/minify',
+          src: '*.svg',
+          dest: 'assets/'
         }]
       }
     },
 
-    // Minifies the scalable vector graphics files.
-    svgmin: {
-      build: {
-        files: [{
-          expand: true,
-          cwd: 'sources/assets',
-          src: '*.svg',
-          dest: 'assets/',
-          ext: '.svg'
-        }]
+    // =========================================================================
+    // If custom styles can be concatenated to one component, then this
+    // block here will replace the placeholder strings with proper <style> tag
+    // beginning and ending.
+    // =========================================================================
+    // replace: {
+    //   custom_styles: {
+    //     src: ['sources/components/custom-styles/tmp/*.css'],
+    //     overwrite: true,
+    //     replacements: [
+    //       {
+    //         from: '/* GRUNT-REPLACE: CUSTOM-STYLES-PREPEND */',
+    //         to: '{% comment %}Template custom styles definitions.{% endcomment %}\n<style data-voog-style>'
+    //       },
+    //       {
+    //         from: '/* GRUNT-REPLACE: CUSTOM-STYLES-APPEND */',
+    //         to: '</style>'
+    //       }
+    //     ]
+    //   }
+    // },
+
+    // Copys the files from the source folders to the layout folders.
+    copy: {
+      assets: {
+        files: [
+          {
+            expand: true,
+            cwd: 'sources/assets/copy',
+            src: '*',
+            dest: 'assets/'
+          }
+        ]
+      },
+
+      images: {
+        files: [
+          {
+            expand: true,
+            cwd: 'sources/images/copy',
+            src: '*',
+            dest: 'images/'
+          }
+        ]
+      },
+
+      javascripts: {
+        files: [
+          {
+            expand: true,
+            cwd: 'sources/javascripts/copy',
+            src: '*',
+            dest: 'javascripts/'
+          }
+        ]
+      },
+
+      // Copies the compiled css files from temporary folder to "components"
+      // folder and renames the files to ""*.tpl".
+      custom_styles: {
+        files: [
+          {
+            expand: true,
+            cwd: 'sources/components/custom-styles/tmp',
+            src: '*.css',
+            dest: 'components',
+            ext: '.tpl'
+          }
+        ]
       }
     },
 
@@ -122,35 +232,57 @@ module.exports = function(grunt) {
 
     // Watches the project for changes and recompiles the output files.
     watch: {
-      js: {
-        files: 'sources/javascripts/*.js',
-        tasks: ['concat:build', 'uglify:build', 'exec:kit:javascripts/*.js']
+      modernizr: {
+        files: 'modernizr-config.json',
+        tasks: ['modernizr_builder:build']
       },
 
-      css: {
+      js_copy: {
+        files: 'sources/javascripts/copy/*.js',
+        tasks: ['copy:javascripts', 'exec:kitmanifest']
+      },
+
+      js_concat: {
+        files: 'sources/javascripts/concat/*.js',
+        tasks: ['concat:build', 'uglify:build', 'exec:kitmanifest']
+      },
+
+      css_main: {
         files: [
           'sources/stylesheets/*.scss',
-          'sources/stylesheets/*/*.scss'
+          'sources/stylesheets/*/*.scss',
         ],
-        tasks: ['sass:build', 'cssmin:build', 'exec:kit:stylesheets/*.css']
+        tasks: ['sass:build_main', 'postcss', 'cssmin:build', 'exec:kitmanifest']
       },
 
-      img: {
-        files: [
-          'sources/images/*.jpg',
-          'sources/images/*.png',
-          'sources/images/*.gif'
-        ],
-        tasks: ['imagemin:build']
+      custom_styles: {
+        files: 'sources/components/custom-styles/*.scss',
+        tasks: ['sass:build_custom_styles', 'copy:custom_styles', 'clean:remove', 'exec:kitmanifest']
       },
 
-      svg: {
-        files: 'sources/assets/*.svg',
-        tasks: ['svgmin:build']
+      img_copy: {
+        files: 'sources/images/copy/*',
+        tasks: [ 'copy:images', 'exec:kitmanifest']
       },
+
+      img_minify: {
+        files: 'sources/images/minify/*',
+        tasks: ['imagemin:build_images', 'exec:kitmanifest']
+      },
+
+      assets_copy: {
+        files: 'sources/assets/copy/*',
+        tasks: ['copy:assets', 'exec:kitmanifest']
+      },
+
+      assets_minify: {
+        files: 'sources/assets/minify/*',
+        tasks: ['imagemin:build_assets', 'exec:kitmanifest']
+      },
+
 
       voog: {
-        files: ['layouts/*.tpl', 'components/*.tpl'],
+        files: ['javascripts/*.js', 'stylesheets/*.css', 'layouts/*.tpl', 'components/*.tpl'],
         options: {
           spawn: false
         }
@@ -158,17 +290,24 @@ module.exports = function(grunt) {
     },
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-modernizr-builder');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-postcss');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // Uncomment if custom styles can be concatenated to one component.
+  // grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-exec');
-  grunt.loadNpmTasks('grunt-modernizr');
-  grunt.loadNpmTasks('grunt-svgmin');
 
-  grunt.registerTask('default', ['modernizr', 'concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'svgmin']);
+  // Default task with text replacement (for automatic <style> tag wrapping).
+  // grunt.registerTask('default', ['clean:reset', 'modernizr_builder', 'concat', 'uglify', 'sass', 'postcss', 'cssmin', 'imagemin', 'replace', 'copy', 'clean:remove']);
+
+  grunt.registerTask('default', ['clean:reset', 'modernizr_builder', 'concat', 'uglify', 'sass', 'postcss:main_styles', 'cssmin', 'imagemin', 'postcss:custom_styles', 'copy', 'clean:remove']);
 
   grunt.event.on('watch', function(action, filepath, target) {
     if (target == 'voog') {
