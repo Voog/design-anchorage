d5f0dd23b7d4267f92b7a001d10eedbdf227eeec
Kaspar Naaber <kaspar.naaber@outlook.com>
2016-04-20 08:59:49 +0300

    Run "postcss" while "watching".

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,7 +257,7 @@ module.exports = function(grunt) {
 
       custom_styles: {
         files: 'sources/components/custom-styles/*.scss',
-        tasks: ['sass:build_custom_styles', 'copy:custom_styles', 'clean:remove', 'exec:kitmanifest']
+        tasks: ['sass:build_custom_styles', 'postcss:custom_styles', 'copy:custom_styles', 'clean:remove', 'exec:kitmanifest']
       },
 
       img_copy: {
